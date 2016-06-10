6b49918ccb3bf0dba58c6802059ef129fe14ef72
jmg2107 <jenjengoo@gmail.com>
2016-06-10 11:02:14 -0700

    completed Tree structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
+  jQuery.extend(newTree, treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +12,23 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var treeBaby = new Tree(value);
+  this.children.push(treeBaby);
 };
 
 treeMethods.contains = function(target) {
+  var doesContain = false;
+  if(this===null)return;
+  if(this.value===target){
+    doesContain = true;
+  }
+  for(var i=0;i<this.children.length;i++){
+    if(doesContain){
+      break;
+    }
+    doesContain = this.children[i].contains(target);
+  }
+  return doesContain;
 };
 
 
