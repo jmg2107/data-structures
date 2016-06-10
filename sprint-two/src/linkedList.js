33f5e9757eff02cfa86c5c4f162c8c4b354cb146
jmg2107 <jenjengoo@gmail.com>
2016-06-10 10:25:15 -0700

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var temp = new Node(value);
+
+    if(list.head === null && list.tail === null){
+      list.head = temp;
+    }
+    else{
+      list.tail.next = temp;
+    }
+    list.tail = temp;
   };
 
   list.removeHead = function() {
+
+    var temp = list.head;
+    if(list.head != null){
+      list.head = list.head.next;
+    }
+    return temp.value;
   };
 
   list.contains = function(target) {
+
+    var node = list.head;
+
+    while(node != null){
+      if(node.value === target){
+        return true;
+      }
+      node = node.next;
+    }
+
+    return false;
   };
 
   return list;
